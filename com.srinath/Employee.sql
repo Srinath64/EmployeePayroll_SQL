bd38953365c67191f8584a8d1d81f1f13a223822
Srinath S <connect2srinath06@gmail.com>
2023-02-07 17:02:30 +0530

    Added : Create Database

--- com.srinath/Employee.sql
+++ com.srinath/Employee.sql
@@ -2,3 +2,25 @@
 
 ----- Welcome to Employee Payroll Database -----
 
+---Use Case 1---
+
+---Ability to create a payroll service database---
+
+mysql> create database payroll_services;
+Query OK, 1 row affected (0.01 sec)
+
+mysql> show databases;
++--------------------+
+| Database           |
++--------------------+
+| information_schema |
+| mysql              |
+| payroll_service    |
+| payroll_services   |
+| performance_schema |
+| sys                |
++--------------------+
+6 rows in set (0.02 sec)
+
+mysql> use payroll_services;
+Database changed
